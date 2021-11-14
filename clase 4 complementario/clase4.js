2dcfdda33e3da6a9359ce51a70ca44ac9faad3fd
Melisa Figueroa <mel.figueroa1407@gmail.com>
2021-11-14 00:00:17 -0300

    clase 5 obj

--- clase 4 complementario/clase4.js
+++ clase 4 complementario/clase4.js
@@ -1,10 +1,38 @@
-function precioIva(precio) {
-  let iva = precio * 0.21;
-  precio = precio + iva;
-  return iva;
+function saludar() {
+  let usser = prompt("Ingrese su nombre");
+  alert("Bienvenido al sistema " + usser);
 }
 
-let precio = parseInt(prompt("Ingrese el precio de costo del producto"));
+saludar();
 
-let resultado = precio + precioIva(precio);
-alert("El precio final del preducto es: " + resultado);
+let precio = parseInt(prompt("Ingrese el valor de su producto"));
+let pago = prompt("Ingrese la forma de pago");
+let ciudad = prompt("Ingrese la ciudad de envío");
+
+function financiacion(pago, precio) {
+  if (pago == "crédito") {
+    let montoFinanciado = precio * 0.15;
+    return montoFinanciado;
+  } else if (pago == "efectivo" && pago == "débito") {
+    let montoFinanciado = precio;
+    return montoFinanciado;
+  }
+}
+
+financiacion();
+
+function costoEnvio(precio, ciudad) {
+  if (ciudad == "Cordoba") {
+    let envio = precio * 0.1;
+    return envio;
+  } else if (ciudad == "Buenos Aires") {
+    let envio = precio * 0.05;
+    return envio;
+  }
+}
+
+costoEnvio();
+
+let resultado =
+  precio + financiacion(pago, precio) + costoEnvio(precio, ciudad);
+alert("El precio final es de: " + resultado);
