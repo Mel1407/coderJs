009063d5939fbd55f760b27da4fd4667219cf266
Melisa Figueroa <mel.figueroa1407@gmail.com>
2022-01-30 04:14:41 -0300

    cambio

--- proyectoFinalJS/code.js
+++ proyectoFinalJS/code.js
@@ -1,3 +1,17 @@
+//<--------- Animación Login -------->
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container__log");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
 /*/<--------- Registro -------->
 
 class usuario {
